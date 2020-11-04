97e348be2d56bada34f3694631c7b75c94f0e1aa
onaxiz <onaxiz@tutanota.com>
2020-11-04 15:36:07 +0300

    hw1

---
+++ my_package/spawner.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python
+import rospy
+
+from turtlesim.srv import Spawn, Kill
+
+def turtlespawn(x,y,theta,name):
+	rospy.wait_for_service('/spawn')
+	spawn_func = rospy.ServiceProxy('/spawn', Spawn)
+	spawn_func(x,y,theta,name)
+	print("Spawned")
+
+def turtlekill(name):
+	rospy.wait_for_service('/kill')
+	kill_func = rospy.ServiceProxy('/kill', Kill)
+	kill_func(name)
+	print("Killed")
+
+class spawner:
+	def __init__(self,x,y,theta,name):
+		rospy.init_node('turtle_spawner')
+		try:
+			turtlespawn(x,y,theta,name)
+		except rospy.service.ServiceException: 
+			turtlekill(name)
+			turtlespawn(x,y,theta,name)
+
+if __name__== '__main__':
+	s = spawner(4.0, 8.0, 0.0,'donatello')
